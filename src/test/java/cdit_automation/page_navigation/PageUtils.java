93032636fe1a54d78b61907e54bac34e42d6d7b2
Ou Guozheng <abc@abc.com>
2019-10-14 22:49:50 +0800

    Add find web elements methods

--- src/test/java/cdit_automation/page_navigation/PageUtils.java
+++ src/test/java/cdit_automation/page_navigation/PageUtils.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class PageUtils {
 
@@ -18,21 +21,43 @@ public class PageUtils {
 
     public WebElement findElement(String cssOrXpath) {
         if ( cssOrXpath.startsWith("//") ) {
-            return findByXpath(cssOrXpath);
+            return findWebElementByXpath(cssOrXpath);
         }
         else {
-            return findByCss(cssOrXpath);
+            return findWebElementByCss(cssOrXpath);
         }
     }
 
-    public WebElement findByXpath(String xpath) {
+    public List<WebElement> findAllWebElements(String cssOrXpath) {
+        List<WebElement> listOfWebElements = null;
+        if ( cssOrXpath.startsWith("//") ) {
+            listOfWebElements = findWebElementsByXpath(cssOrXpath);
+        } else {
+            listOfWebElements = findWebElementsByCss(cssOrXpath);
+        }
+
+        return listOfWebElements;
+    }
+
+    public List<WebElement> findWebElementsByXpath(String xpath) {
+        List<WebElement> webElements = driverManager.getDriver().findElements(By.xpath(xpath));
+        return webElements;
+    }
+
+    public WebElement findWebElementByXpath(String xpath) {
         WebElement webElement = driverManager.getDriver().findElement(By.xpath(xpath));
         return webElement;
     }
 
-    public WebElement findByCss(String css) {
+    public List<WebElement> findWebElementsByCss(String css) {
+        List<WebElement> webElements = driverManager.getDriver().findElements(By.cssSelector(css));
+        return webElements;
+    }
+
+    public WebElement findWebElementByCss(String css) {
         WebElement webElement = driverManager.getDriver().findElement(By.cssSelector(css));
 
         return webElement;
     }
+
 }
