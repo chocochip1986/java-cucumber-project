f34d4aab81e133e290721c78b4070e42df569145
Ou Guozheng <abc@abc.com>
2019-11-13 16:24:24 +0800

    [#GRYFFINDOR-590] - added person detail repo and model

--- src/test/java/cdit_automation/data_helpers/AbstractService.java
+++ src/test/java/cdit_automation/data_helpers/AbstractService.java
@@ -6,36 +6,22 @@ import cdit_automation.repositories.FileDetailRepo;
 import cdit_automation.repositories.FileReceivedRepo;
 import cdit_automation.repositories.IncomingRecordRepo;
 import cdit_automation.repositories.NationalityRepo;
+import cdit_automation.repositories.PersonDetailRepo;
 import cdit_automation.repositories.PersonIdRepo;
 import cdit_automation.repositories.PersonRepo;
 import cdit_automation.utilities.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class AbstractService {
-    @Autowired
-    protected DateUtils dateUtils;
+    @Autowired protected DateUtils dateUtils;
 
-    @Autowired
-    protected PersonIdRepo personIdrepo;
-
-    @Autowired
-    protected PersonRepo personRepo;
-
-    @Autowired
-    protected NationalityRepo nationalityRepo;
-
-    @Autowired
-    protected BatchRepo batchRepo;
-
-    @Autowired
-    protected FileDetailRepo fileDetailRepo;
-
-    @Autowired
-    protected FileReceivedRepo fileReceivedRepo;
-
-    @Autowired
-    protected IncomingRecordRepo incomingRecordRepo;
-
-    @Autowired
-    protected ErrorMessageRepo errorMessageRepo;
+    @Autowired protected PersonIdRepo personIdrepo;
+    @Autowired protected PersonRepo personRepo;
+    @Autowired protected NationalityRepo nationalityRepo;
+    @Autowired protected BatchRepo batchRepo;
+    @Autowired protected FileDetailRepo fileDetailRepo;
+    @Autowired protected FileReceivedRepo fileReceivedRepo;
+    @Autowired protected IncomingRecordRepo incomingRecordRepo;
+    @Autowired protected ErrorMessageRepo errorMessageRepo;
+    @Autowired protected PersonDetailRepo personDetailRepo;
 }
