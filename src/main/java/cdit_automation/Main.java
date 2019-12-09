60cd9e8cd1f526bde82bfc5c85f29a967a3cefdd
Ou Guozheng <abc@abc.com>
2019-12-09 17:35:56 +0800

    [#GRYFFINDOR-846] - Added a src/java/main directory

---
+++ src/main/java/cdit_automation/Main.java
@@ -0,0 +1,8 @@
+package cdit_automation;
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+//        String[] arguments = {};
+//        io.cucumber.core.cli.Main.main(arguments);
+    }
+}
