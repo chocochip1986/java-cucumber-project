fbaad4584f75dc74f72ad243b86678abf6851c88
Ou Guozheng <abc@abc.com>
2020-03-05 13:57:20 +0800

    [#RVC-31] - Removed unncessary import

--- src/main/java/cdit_automation/data_helpers/factories/FileDataProcessingFactory.java
+++ src/main/java/cdit_automation/data_helpers/factories/FileDataProcessingFactory.java
@@ -28,7 +28,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
