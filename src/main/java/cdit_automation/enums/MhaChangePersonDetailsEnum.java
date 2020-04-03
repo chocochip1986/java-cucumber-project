9b5af7feeb5f43d48feac7a45a8fdf71e9ac30ac
lucas_yap <54-lucas_yap@users.noreply.gitlab.cpfb.gds-gov.tech>
2020-04-03 06:52:43 +0000

    Chore/add person detail change scenario

---
+++ src/main/java/cdit_automation/enums/MhaChangePersonDetailsEnum.java
@@ -0,0 +1,31 @@
+package cdit_automation.enums;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Slf4j
+public enum MhaChangePersonDetailsEnum {
+  Person_Detail("PD"),
+  Person_Name("PN"),
+  Person_Gender("PG");
+
+  private String value;
+
+  MhaChangePersonDetailsEnum(String s) {
+    this.value = s;
+  }
+
+  public String getValue() {
+    return this.value;
+  }
+
+  public static MhaChangePersonDetailsEnum fromString(String str) {
+    for (MhaChangePersonDetailsEnum enumVal : MhaChangePersonDetailsEnum.values()) {
+      if (enumVal.getValue().equals(str)) {
+        return enumVal;
+      }
+    }
+    return null;
+  }
+}
